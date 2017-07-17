7484583c503844fa66f6d8816af30a50a8070e22
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-07-17 18:36:38 -0300

    Initial commit

--- src/Dummy.cpp
+++ src/Dummy.cpp
@@ -1,10 +1,13 @@
 #include "Dummy.hpp"
 #include <iostream>
+#include <plain_package/Dummy.hpp>
 
 using namespace std;
 using namespace rock_library_no_public_dependencies;
 
 void DummyClass::welcome()
 {
+    plain_package::DummyClass dummy;
+    dummy.welcome();
     cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
 }
--- src/Dummy.hpp
+++ src/Dummy.hpp
@@ -1,5 +1,5 @@
-#ifndef _DUMMYPROJECT_DUMMY_HPP_
-#define _DUMMYPROJECT_DUMMY_HPP_
+#ifndef _ROCK_LIBRARY_NO_PUBLIC_DEPENDENCIES_DUMMY_HPP_
+#define _ROCK_LIBRARY_NO_PUBLIC_DEPENDENCIES_DUMMY_HPP_
 
 namespace rock_library_no_public_dependencies
 {
@@ -15,4 +15,4 @@ namespace rock_library_no_public_dependencies
 
 } // end namespace rock_library_no_public_dependencies
 
-#endif // _DUMMYPROJECT_DUMMY_HPP_
+#endif // _ROCK_LIBRARY_NO_PUBLIC_DEPENDENCIES_DUMMY_HPP_
